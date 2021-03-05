3edba0b063efd1ce73ff4dd260cc81641d077827
darnocer <darnocer@gmail.com>
2021-03-05 08:09:05 -0700

    starting content, navigation pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,6 +46,27 @@ module.exports = {
         siteUrl: `https://rocketdocs.netlify.com`,
       },
     },
+    // {
+    //   resolve: "@mkitio/gatsby-theme-password-protect",
+    //   options: {
+    //     password: "sUp3rS3cR3t", // delete or `undefined` to disable password protection
+    //   },
+    // },
     `gatsby-plugin-offline`,
+    // {
+    //   resolve: "gatsby-plugin-mdx",
+    //   options: {
+    //     root: __dirname,
+    //     gatsbyRemarkPlugins: [
+    //       {
+    //         resolve: "gatsby-remark-images",
+    //         options: {
+    //           maxWidth: 500,
+    //           linkImagesToOriginal: false,
+    //         },
+    //       },
+    //     ],
+    //   },
+    // },
   ],
 };
---
+++ src/@rocketseat/gatsby-theme-docs/components/Logo.js
@@ -0,0 +1,6 @@
+import React from "react";
+import logo from "../assets/Logo.jpeg";
+
+const Logo = () => <img src={logo} alt="TUF logo" style={{ width: 180 }} />;
+
+export default Logo;
